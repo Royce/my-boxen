707b312c6c809c144d2b440809326c813aeaa3f1
Royce Townsend <git@roycetownsend.com>
2014-12-23 18:04:08 +0800

    Tidy up OS X defaults.

--- modules/people/manifests/Royce/osx_defaults.pp
+++ modules/people/manifests/Royce/osx_defaults.pp
@@ -1,22 +1,31 @@
 class people::royce::osx_defaults {
 
-  # Relay fast key repeat after 10 ms
+  #
+  # Typing
+  #
   include osx::global::disable_key_press_and_hold
   include osx::global::key_repeat_rate
-  class { 'osx::global::key_repeat_delay':
-    delay => 10
-  }
-
+  include osx::global::key_repeat_delay
   include osx::global::enable_keyboard_control_access
+  include osx::keyboard::capslock_to_control
+  include osx::global::enable_standard_function_keys
+
+  #
+  # Files
+  #
   include osx::global::expand_print_dialog
   include osx::global::expand_save_dialog
   include osx::finder::show_all_on_desktop
   include osx::finder::unhide_library
-
   include osx::disable_app_quarantine
-  
   include osx::no_network_dsstores
 
+  # Misc
+  include osx::safari::enable_developer_mode
+
+  #
+  # Screenshots
+  #
   boxen::osx_defaults { 'Save screenshots to dropbox':
     ensure => present,
     domain => 'com.apple.screencapture',
