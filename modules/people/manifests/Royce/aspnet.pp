7758710b38a888b90e94b53c5ce8fc3b7c9798ff
Royce Townsend <git@roycetownsend.com>
2015-10-20 14:06:23 +1100

    Remove nodejs; apps; K; sublime
    boxen was not good and managing them

--- modules/people/manifests/Royce/aspnet.pp
+++
@@ -1,9 +0,0 @@
-class people::royce::aspnet (
-  $my_sourcedir = $people::royce::params::my_sourcedir
-) {
-
-  repository { "${my_sourcedir}/OmniSharpServer":
-    source => 'nosami/OmniSharpServer',
-  }
-
-}
